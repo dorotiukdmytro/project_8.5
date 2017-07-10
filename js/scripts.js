3b0f54160480e5939e4c053e064ed4c638b47431
Dmytro <ddorotiuk@gmail.com>
2017-07-10 20:03:47 +0200

    Add basic structure

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// scripts.js
+
+// alert("test");
+// alert("test"); is working
+// new functions will be add
+var name = prompt('Enter your name');
+alert('Hello, ' + name);
+console.log('Hello, ' + name);
+
+var
+	a = prompt("Enter parametr base of triangle"),
+	h = prompt("Enter parametr height of triangle"),
+	triangleArea = a*h/2;
+alert("Triangle field is " + triangleArea);
+console.log("Triangle field with base a: " + a + " and height h: " + h + " is equal to: " + triangleArea);
